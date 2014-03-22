e1d5b2aa399d85e19c8310982e193a6b0b82252c
rojun <rojun.itu@gmail.com>
2014-03-22 13:58:50 +0200

    Nicer front screen. Included two banners (mdpi and xdpi sizes).

--- gen/travist/pack/R.java
+++ gen/travist/pack/R.java
@@ -18,12 +18,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int travist_icon=0x7f020001;
+        public static final int travist_banner=0x7f020001;
+        public static final int travist_icon=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080005;
         public static final int button1=0x7f080000;
         public static final int button2=0x7f080001;
+        public static final int imageView1=0x7f080004;
         public static final int main_view_layout=0x7f080003;
         public static final int tv=0x7f080002;
     }
