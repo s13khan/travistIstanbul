1747ac9a84349b196390818cc057ec2c6a6d73a1
rojun <rojun.itu@gmail.com>
2014-03-22 10:43:43 +0200

    map-file in assets. Loads it. Shows offline map.

--- src/travist/pack/TravistIstanbulActivity.java
+++ src/travist/pack/TravistIstanbulActivity.java
@@ -1,7 +1,9 @@
 package travist.pack;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TravistIstanbulActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,6 +12,10 @@ public class TravistIstanbulActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-
+    }
+    
+    public void openMap(View view) {
+    	Intent intent = new Intent(this, TestOfflineMapActivity.class);
+    	startActivity(intent);
     }
 }
